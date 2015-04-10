c7b31266b228c8c7e15004f1b7e18135ae241544
EricSchles <ericschles@gmail.com>
2015-04-09 17:31:09 -0700

    adding docs

---
+++ README.md
@@ -0,0 +1,24 @@
+#Intro
+
+The database `mongo_esc` is an extremely simple database.
+
+##The Basics:
+
+###Connecting
+
+>>> from database_store import DB
+>>> db = DB()
+>>> db.connect()
+
+###Putting Data In && Getting Data Out
+
+>>> from database_store import DB
+>>> db = DB()
+>>> db.connect()
+>>> db.change_db("eric")
+>>> db.save_data("schles",{"answer to everything":42})
+>>> db.get_data("schles",document={"answer to everything":42})
+[{u'answer to everything': 42}]
+
+
+
--- database_store.py
+++ database_store.py
@@ -3,70 +3,76 @@ import random
 import hashlib
 from glob import glob
 import os
-def save_data(collection,document):
-    if os.getcwd() == "dbs":
-        return "No DB specified" #make this an exception
-    else:
-        if not os.path.exists(collection):
-            os.mkdir(collection)
-        os.chdir(collection)
-        file_name = hashlib.sha256(str(random.randint(0,1000000))).hexdigest()
-        while file_name in glob("*"):
-            file_name = hashlib.sha256(str(random.randint(0,1000000))).hexdigest()
-        json.dump(document,open(str(file_name),"w"))
-        os.chdir("../")
 
-def get_data(collection,document=None,keys=None,values=None,ids=None):
-    search_params = [keys,values,ids]
-    to_return = []
-    to_search = []
-    if document == None:
-        os.chdir(collection)
-        if any(search_params):
-            for doc in glob("*"): to_search.append(json.load(open(doc,"r")))
-            if keys:
-                for doc in to_search:
-                    for key in doc.keys():
-                        if key in keys:
-                            to_return.append({key:doc[key]})
-            if values:
-                for doc in to_search:
-                    for key in doc.keys():
-                        if value == doc[key]:
-                            to_return.append({key:doc[key]})
-            if ids:
-                for Id in ids:
-                    if Id in glob("*"):
-                        to_return.append(json.load(open(Id),"r"))
-    elif document:
-        for doc in glob("*"): to_search.append(json.load(open(doc,"r")))
-        for doc in to_search:
-            if doc == document:
-                if any(search_params):
-                    if keys:
+class DB:
+    def __init__(self):
+        pass
+
+    def save_data(self,collection,document):
+        if os.getcwd() == "dbs":
+            return "No DB specified" #make this an exception
+        else:
+            if not os.path.exists(collection):
+                os.mkdir(collection)
+            os.chdir(collection)
+            file_name = hashlib.sha256(str(random.randint(0,1000000))).hexdigest()
+            while file_name in glob("*"):
+                file_name = hashlib.sha256(str(random.randint(0,1000000))).hexdigest()
+            json.dump(document,open(str(file_name),"w"))
+            os.chdir("../")
+
+    def get_data(self,collection,document=None,keys=None,values=None,ids=None):
+        search_params = [keys,values,ids]
+        to_return = []
+        to_search = []
+        if document == None:
+            os.chdir(collection)
+            if any(search_params):
+                for doc in glob("*"): to_search.append(json.load(open(doc,"r")))
+                if keys:
+                    for doc in to_search:
                         for key in doc.keys():
                             if key in keys:
                                 to_return.append({key:doc[key]})
-                    if values:
+                if values:
+                    for doc in to_search:
                         for key in doc.keys():
                             if value == doc[key]:
                                 to_return.append({key:doc[key]})
-                else:
-                    to_return.append(doc)
-    return to_return
+                if ids:
+                    for Id in ids:
+                        if Id in glob("*"):
+                            to_return.append(json.load(open(Id),"r"))
+        elif document:
+            os.chdir(collection)
+            for doc in glob("*"): to_search.append(json.load(open(doc,"r")))
+            for doc in to_search:
+                if doc == document:
+                    if any(search_params):
+                        if keys:
+                            for key in doc.keys():
+                                if key in keys:
+                                    to_return.append({key:doc[key]})
+                        if values:
+                            for key in doc.keys():
+                                if value == doc[key]:
+                                    to_return.append({key:doc[key]})
+                    else:
+                        to_return.append(doc)
+        return to_return
 
-def create_db(db_name):
-    if db_name == "dbs":
-        return "can't name your datastore dbs" #make this an exception
-    os.mkdir(db_name)
-
-def change_db(db_name):
-    if not os.path.exists(db_name):
-        if not create_db(db_name):
+    def create_db(self,db_name):
+        if db_name == "dbs":
             return "can't name your datastore dbs" #make this an exception
-    os.chdir(db_name)
+        os.mkdir(db_name)
 
-def connect():
-    if not os.path.exists("dbs"):
-        os.mkdir("dbs")
-    os.chdir("dbs")
+    def change_db(self,db_name):
+        if not os.path.exists(db_name):
+            if not self.create_db(db_name):
+                return "can't name your datastore dbs" #make this an exception
+        os.chdir(db_name)
+
+    def connect(self):
+        if not os.path.exists("dbs"):
+            os.mkdir("dbs")
+        os.chdir("dbs")
